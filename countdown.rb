f5f79dddc3923fb2744c7c01f99c75e489a6d5f5
Jamil Seddiqi <jamilsediq@gmail.com>
2019-12-18 23:20:15 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,8 @@
-#write your code here
-
-def countdown
+#write your code her
+def countdown(integer)
+  while integer > 0
+    puts "#{integer} SECOND(S)!"
+    count -= 0
+  end
+   "Happy New Year"
 end
